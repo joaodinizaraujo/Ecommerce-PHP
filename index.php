3ebce1d4cc6e008a550ba413f46bea268d74ab90
joaodinizaraujo <joaovictorclassroom@gmail.com>
2024-04-28 00:19:59 -0300

    autoload classes

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
 
+use joaodinizaraujo\DB\Sql;
 use Slim\Slim;
 
 require_once("vendor/autoload.php");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-	echo "OK";
+	$sql = new Sql();
+    $results = $sql->select("SELECT * FROM tb_users");
+    echo json_encode($results);
 });
 
 $app->run();
---
+++ vendor/joaodinizaraujo/php-classes/src/DB/Sql.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace joaodinizaraujo\DB;
+
+class Sql {
+	private $conn;
+
+	public function __construct(){
+		$this->conn = new \PDO(
+			"mysql:dbname=".$_ENV["DBNAME"].";host=".$_ENV["DBHOSTNAME"],
+			$_ENV["DBUSERNAME"],
+			$_ENV["DBPASSWORD"]
+		);
+	}
+
+	private function setParams($statement, $parameters = array()){
+		foreach ($parameters as $key => $value) {
+			$this->bindParam($statement, $key, $value);
+		}
+	}
+
+	private function bindParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
+	}
+
+	public function query($rawQuery, $params = array()){
+		$stmt = $this->conn->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+	}
+
+	public function select($rawQuery, $params = array()):array{
+		$stmt = $this->conn->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+}
+
+ ?>
